f17e0056919fba6ecc87bbb0c008c48f0ee3ed5d
najmakhonsa <najmakhonsa@gmail.com>
2026-06-18 15:10:22 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  vite: {
-    ssr: {
-      external: ["nitro"],
-    },
-  },
+  nitro: true,
 });
